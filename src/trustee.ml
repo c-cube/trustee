5083f716a67295497c5569a94f2f96096976050e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-01-19 19:17:12 -0600

    wip: add `Thm.t` and some axioms

--- src/trustee.ml
+++ src/trustee.ml
@@ -6,6 +6,10 @@ let pp_list ppx out l =
 
 let invalid_argf msg = Fmt.kasprintf invalid_arg msg
 
+(** {2 Identifiers}
+
+    Identifiers represent a given logic symbols. A handful are predefined,
+    the other ones are introduced by the user or the problem to check. *)
 module ID : sig
   type t
 
@@ -31,7 +35,6 @@ module ID : sig
 end = struct
   type builtin =
     | Eq | Not | And | Or | Imply | Bool
-
 
   type view =
     | Builtin of builtin
@@ -71,6 +74,9 @@ end = struct
   let bool = Builtin Bool
 end
 
+(** {2 Types}
+
+    Polymorphic types. *)
 module Ty : sig
   type var = ID.t
   type t
@@ -80,16 +86,20 @@ module Ty : sig
     | App of ID.t * t list
     | Var of var
 
+  val bool : t
   val app : ID.t -> t list -> t
   val const : ID.t -> t
   val var : var -> t
   val arrow : t -> t -> t
   val arrow_l : t list -> t -> t
+  val (@->) : t -> t -> t
 
   val view : t -> view
   val equal : t -> t -> bool
   val hash : t -> int
   val pp : t Fmt.printer
+
+  val is_bool : t -> bool
 end = struct
   type var = ID.t
 
@@ -143,8 +153,16 @@ end = struct
   let app c l : t = make_ (App (c, l))
   let const c : t = app c []
   let var v : t = make_ (Var v)
+  let bool : t = const ID.bool
+  let (@->) = arrow
+
+  let is_bool ty = equal bool ty
 end
 
+(** {2 Typed Variables}
+
+    A variable is a typed identifier. It can represent bound variables or
+    defined/axiomatized constants. *)
 module Var : sig
   type t = private {name: ID.t; ty: Ty.t}
 
@@ -164,7 +182,10 @@ end = struct
   let pp out v = Fmt.fprintf out "(@[%a : %a@])" ID.pp v.name Ty.pp v.ty
 end
 
-module Expr : sig
+(** {2 Terms}
+
+    Logical expressions and formulas. *)
+module Term : sig
   type t
 
   type view =
@@ -174,12 +195,21 @@ module Expr : sig
 
   val view : t -> view
   val ty : t -> Ty.t
+  val equal : t -> t -> bool
+  val compare : t -> t -> int
+  val hash : t -> int
+  val pp : t Fmt.printer
 
   val app : t -> t -> t
   val app_l : t -> t list -> t
   val var : Var.t -> t
   val lam : Var.t -> t -> t
   val lam_l : Var.t list -> t -> t
+  val eq_const : Ty.t -> t
+  val eq : t -> t -> t
+
+  module Set : Set.S with type elt = t
+  module Tbl : Hashtbl.S with type key = t
 end = struct
   type t = {
     mutable id: int;
@@ -196,6 +226,7 @@ end = struct
   let hash a = CCHash.int a.id
   let view t = t.view
   let ty t = t.ty
+  let compare a b = CCInt.compare a.id b.id
 
   let rec pp out t =
     match t.view with
@@ -238,4 +269,99 @@ end = struct
   let lam v body : t = make_ (Lam (v,body)) (Ty.arrow (Var.ty v) (ty body))
   let lam_l vs body : t = List.fold_right lam vs body
   let app_l f l = List.fold_left app f l
+
+  let eq_const ty : t = var (Var.make ID.eq Ty.(ty @-> ty @-> bool))
+  let eq a b : t =
+    if not (Ty.equal a.ty b.ty) then (
+      invalid_argf "cannot build equality between %a@ and %a: incompatible types" pp a pp b
+    );
+    app (app (eq_const a.ty) a) b
+
+  module As_key = struct
+    type nonrec t = t
+    let equal = equal
+    let hash = hash
+    let compare=compare
+  end
+  module Set = Set.Make(As_key)
+  module Tbl = Hashtbl.Make(As_key)
+end
+
+
+(** {2 Theorems}
+
+    Here lies the core of the LCF system: the only values of type
+    {!Thm.t} that can be constructed are valid consequences of the
+    logic's axioms. *)
+module Thm : sig
+  type t
+
+  val pp : t Fmt.printer
+  val concl : t -> Term.t
+  val hyps : t -> Term.Set.t
+
+  (** Creation of new terms *)
+
+  val refl : Term.t -> t
+  (** [refl t] is [ |- t=t ] *)
+
+  val assume : Term.t -> t
+  (** [assume F] is [F |- F] *)
+
+  val mp : t -> t -> t
+  (** [mp (F1, a |- b) (F2, b |- c)] is [F1, F2, a |- c] *)
+
+  val cong : Term.t -> Term.t list -> Term.t list -> t
+  (** [cong f l1 l2] makes the congruence axiom for [∧l1=l2 ==> f l1=f2 l2] *)
+end = struct
+  type t = {
+    concl: Term.t;
+    hyps: Term.Set.t;
+  }
+
+  let concl self = self.concl
+  let hyps self = self.hyps
+  let pp out self =
+    if Term.Set.is_empty self.hyps then (
+      Fmt.fprintf out "@[|- %a@]" Term.pp self.concl
+    ) else (
+      Fmt.fprintf out "@[%a@ |- %a@]"
+        Fmt.(list ~sep:(return ",@ ") Term.pp) (Term.Set.elements self.hyps) Term.pp self.concl
+    )
+
+  let make_ concl hyps : t = {concl; hyps}
+
+  let assume t : t =
+    if not (Ty.is_bool @@ Term.ty t) then (
+      invalid_argf "assume: needs boolean term, not %a" Term.pp t
+    );
+    make_ t (Term.Set.singleton t)
+
+  let refl t : t = make_ (Term.eq t t) Term.Set.empty
+
+  let cong f l1 l2 : t =
+    if List.length l1 <> List.length l2 then (
+      invalid_arg "cong: incompatible length"
+    );
+    let app1 = Term.app_l f l1 in
+    let app2 = Term.app_l f l2 in
+    if not (Ty.equal (Term.ty app1) (Term.ty app2)) then (
+      invalid_argf "cong: terms %a and %a have incompatible types"
+        Term.pp app1 Term.pp app2
+    );
+    make_ (Term.eq app1 app2)
+      (List.map2 Term.eq l1 l2 |> Term.Set.of_list)
+
+  (* TODO: remove refl hyps, using filter? *)
+
+  let mp a b : t =
+    let {concl=concl_b; hyps=hyps_b} = b in
+    if Term.Set.mem (concl a) hyps_b then (
+      let new_hyps =
+        Term.Set.union (hyps a) (Term.Set.remove (concl a) hyps_b)
+      in
+      make_ concl_b new_hyps
+    ) else (
+      invalid_argf "mp: %a@ does not belong in hyps of %a" Term.pp (concl a) pp b
+    )
 end
