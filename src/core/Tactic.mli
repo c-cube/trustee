f40934485a96db7adbbb7088eda2420ac217f5e8
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-05-09 23:05:24 -0400

    remove ocaml code

--- src/core/Tactic.mli
+++
@@ -1,9 +0,0 @@
-module Make(C : Core.S) : sig
-  open C.KoT
-  module Goal : module type of struct include Goal.Make(C) end
-
-  type goal = Goal.t
-  type subgoals = goal list * (Thm.t list -> Thm.t)
-
-  type t = goal -> subgoals
-end
