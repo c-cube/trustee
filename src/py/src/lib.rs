a467a90cb3222311ab389e149c4194973d201ef0
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-05-13 23:19:47 -0400

    wip: start python bindings  using cpython-rust

---
+++ src/py/src/lib.rs
@@ -0,0 +1,102 @@
+// see https://github.com/dgrunwald/rust-cpython
+
+#[macro_use]
+use cpython::{
+    py_class, py_module_initializer, PyErr, CompareOp, PyObject, PyResult,
+    Python,
+};
+use std::{sync::Arc, sync::Mutex};
+use trustee::kernel_of_trust as k;
+
+// add bindings to the generated python module
+// N.B: names: "rust2py" must be the name of the `.so` or `.pyd` file
+py_module_initializer!(
+    trustee_py,
+    inittrustee_py,
+    PyInit_trustee_py,
+    |py, m| {
+        m.add(py, "__doc__", "Python bindings for trustee.")?;
+        m.add_class::<ExprManager>(py)?;
+        m.add_class::<Expr>(py)?;
+        Ok(())
+    }
+);
+
+py_class!(class Expr |py| {
+    data expr: k::Expr;
+    data em: Arc<Mutex<k::ExprManager>>;
+
+    def __repr__(&self) -> PyResult<String> {
+        let e = self.expr(py);
+        Ok(e.to_string())
+    }
+
+    def __str__(&self) -> PyResult<String> {
+        let e = self.expr(py);
+        Ok(e.to_string())
+    }
+
+    def __call__(&self, arg : Expr) -> PyResult<Expr> {
+        let em = self.em(py);
+        let mut g_em = em.lock().unwrap();
+        let e = g_em.mk_app(self.expr(py).clone(), arg.expr(py).clone());
+        drop(g_em);
+        Expr::create_instance(py, e, em.clone())
+    }
+
+    /* FIXME
+    def __richcmp__(&self, other : Expr, op : CompareOp) -> PyResult<bool> {
+        match op {
+            CompareOp::Eq => Ok(self.expr(py) == other.expr(py)),
+            _ => Err(PyErr::new::<cpython::exc::NotImplementedError,_>(py, "not implemented"))
+        }
+    }
+    */
+});
+
+// TODO: theorem
+
+py_class!(class ExprManager |py| {
+    data em: Arc<Mutex<k::ExprManager>>;
+
+    def __new__(_cls) -> PyResult<ExprManager> {
+        ExprManager::create_instance(py, Arc::new(Mutex::new(k::ExprManager::new())))
+    }
+
+    def ty(&self) -> PyResult<Expr> {
+        let em = self.em(py).lock().unwrap();
+        let ty = em.mk_ty();
+        Expr::create_instance(py, ty, self.em(py).clone())
+    }
+});
+
+/*
+impl PythonObject for AExprManager {
+    fn as_object(&self) -> &cpython::objects::object::PyObject {
+        todo!()
+    }
+
+    fn into_object(self) -> cpython::objects::object::PyObject {
+        todo!()
+    }
+
+    unsafe fn unchecked_downcast_from(
+        _: cpython::objects::object::PyObject,
+    ) -> Self {
+        todo!()
+    }
+
+    unsafe fn unchecked_downcast_borrow_from(
+        _: &cpython::objects::object::PyObject,
+    ) -> &Self {
+        todo!()
+    }
+}
+
+impl ToPyObject for AExprManager {
+    type ObjectType = AExprManager;
+    fn to_py_object(&self, py: Python) -> Self::ObjectType {
+        self.clone()
+    }
+}
+*/
