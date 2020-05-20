c7839a6b2fafe4113bc7bb5aff51000452998eb3
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-05-20 18:45:08 -0400

    OT: add function defs and thms

---
+++ data/opentheory/function-def-1.20/function-def.thy
@@ -0,0 +1,12 @@
+name: function-def
+version: 1.20
+description: Definition of function operators and combinators
+author: Joe Leslie-Hurd <joe@gilith.com>
+license: HOLLight
+requires: bool
+show: "Data.Bool"
+show: "Function"
+
+main {
+  article: "function-def.art"
+}
