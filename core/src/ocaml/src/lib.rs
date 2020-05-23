073dded2806eefa45f4263c5299bd427daa6cb9b
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-05-22 23:17:59 -0400

    wip: ocaml bindings

--- core/src/ocaml/src/lib.rs
+++ core/src/ocaml/src/lib.rs
@@ -1,4 +1,4 @@
-use ocaml::{FromValue, Pointer};
+use ocaml::{FromValue, Pointer, ToValue, Value};
 use std::sync::{Arc, Mutex};
 use trustee::kernel_of_trust as k;
 
@@ -31,6 +31,39 @@ pub fn trustee_expr_ty(v: Pointer<Expr>) -> Pointer<Expr> {
         None => ocaml::Error::raise_failure("expr has no type"),
         Some(ty) => Pointer::alloc_custom(Expr(ty.clone())),
     }
+}
+
+// ==== Thm ====
+
+/// A wrapper around an expression.
+#[derive(Debug, Clone)]
+pub struct Thm(k::Thm);
+
+unsafe extern "C" fn trustee_finalize_thm(v: ocaml::Value) {
+    let ptr: Pointer<Thm> = Pointer::from_value(v);
+    ptr.drop_in_place()
+}
+
+ocaml::custom_finalize!(Thm, trustee_finalize_thm);
+
+#[ocaml::func]
+pub fn trustee_thm_to_string(v: Pointer<Thm>) -> String {
+    v.as_ref().0.to_string()
+}
+
+#[ocaml::func]
+pub fn trustee_thm_concl(v: Pointer<Thm>) -> Pointer<Expr> {
+    let e = v.as_ref().0.concl();
+    Pointer::alloc_custom(Expr(e.clone()))
+}
+
+#[ocaml::func]
+pub fn trustee_thm_hyps(v: Pointer<Thm>) -> Vec<Pointer<Expr>> {
+    let mut res = vec![];
+    for x in v.as_ref().0.hyps() {
+        res.push(Pointer::alloc_custom(Expr(x.clone())))
+    }
+    res
 }
 
 // ==== Context ====
@@ -109,3 +142,52 @@ pub fn trustee_mk_eq_app(
     let t = ctx.mk_eq_app(e1.as_ref().0.clone(), e2.as_ref().0.clone())?;
     Ok(Pointer::alloc_custom(Expr(t)))
 }
+
+#[ocaml::func]
+pub fn trustee_thm_assume(
+    mut ctx: Pointer<Ctx>,
+    e: Pointer<Expr>,
+) -> Result<Pointer<Thm>, ocaml::Error> {
+    let mut ctx = ctx.as_mut().0.lock().unwrap();
+    let t = ctx.thm_assume(&e.as_ref().0);
+    Ok(Pointer::alloc_custom(Thm(t)))
+}
+
+/* TODO
+// === OPEN THEORY ====
+
+/// A wrapper around the context.
+#[derive(Debug, Clone)]
+pub struct OT(Arc<Mutex<k::ExprManager>>);
+
+unsafe extern "C" fn trustee_finalize_ot(v: ocaml::Value) {
+    let ptr: Pointer<OT> = Pointer::from_value(v);
+    ptr.drop_in_place()
+}
+
+ocaml::custom_finalize!(Ctx, trustee_finalize_ctx);
+
+/// Parse OpenTheory
+#[ocaml::func]
+pub fn trustee_ot_parse(mut ctx: Pointer<Ctx>, s: &str) -> Vec<Pointer<Thm>> {
+    let mut ctx = ctx.as_mut().0.lock().unwrap();
+    let mut vm = trustee::open_theory::VM::new(&mut ctx);
+    let mut buf = std::io::BufReader::new(s.as_bytes());
+    eprintln!("reading string (len {})", s.as_bytes().len());
+    vm.parse_str(&mut buf).unwrap_or_else(|e| {
+        let msg = format!("trustee_parse_ot: {}", e);
+        ocaml::Error::raise_failure(&msg)
+    });
+    eprintln!("got OT article");
+    let article = vm.into_article();
+    let (v1, v2, v3) = article.get(&mut ctx);
+    //let v1: Vec<_> =
+    //    v1.into_iter().map(|e| Pointer::alloc_custom(Expr(e))).collect();
+    //let v2: Vec<_> =
+    //    v2.into_iter().map(|e| Pointer::alloc_custom(Thm(e))).collect();
+    let v3: Vec<_> =
+        v3.into_iter().map(|e| Pointer::alloc_custom(Thm(e))).collect();
+    //(v1.to_value(), v2.to_value(), v3.to_value())
+    v3
+}
+*/
--- core/src/ocaml/src/trustee.ml
+++ core/src/ocaml/src/trustee.ml
@@ -1,6 +1,8 @@
 
 type expr
 type ty = expr
+type thm
+type ctx
 
 module Expr = struct
   type t = expr
@@ -10,12 +12,35 @@ module Expr = struct
   let pp out e = Format.pp_print_string out (to_string e)
 end
 
-type ctx
+module Thm = struct
+  type t = thm
+  external to_string : t -> string = "trustee_thm_to_string"
+  external concl : t -> expr = "trustee_thm_concl"
+  external hyps : t -> expr array = "trustee_thm_hyps"
+  let hyps_l th = Array.to_list (hyps th)
+  let pp out e = Format.fprintf out "@[%a@]" Format.pp_print_text (to_string e)
+end
 
 module Ctx = struct
   type t = ctx
   external create : unit -> t = "trustee_new_ctx"
 end
+
+(* TODO
+module OpenTheory = struct
+  type vm
+  external create : ctx -> vm = "trustee_ot_create"
+  external parse_ot_str : vm -> string -> unit = "trustee_ot_parse"
+  let parse_ot_file vm f =
+    let ic = open_in f in
+    let n = in_channel_length ic in
+    let content = really_input_string ic n in
+    parse_ot_str vm content
+  external defs : vm -> expr array = "trustee_ot_defs"
+  external axioms : vm -> thm array = "trustee_ot_axioms"
+  external theorems : vm -> thm array = "trustee_ot_theorems"
+end
+   *)
 
 external mk_type : ctx -> ty = "trustee_mk_type"
 external mk_bool : ctx -> ty = "trustee_mk_bool"
@@ -23,3 +48,5 @@ external mk_var : ctx -> string -> ty -> expr = "trustee_mk_var"
 external mk_app : ctx -> expr -> expr -> expr = "trustee_mk_app"
 external mk_arrow : ctx -> expr -> expr -> expr = "trustee_mk_arrow"
 external mk_eq : ctx -> expr -> expr -> expr = "trustee_mk_eq_app"
+
+external assume : ctx -> expr -> thm = "trustee_thm_assume"
