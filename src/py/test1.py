5ae45463c5646b80ff5e592b5478d5ec08c9a045
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-06-02 00:14:17 -0400

    wip: small script to test python interface

---
+++ src/py/test1.py
@@ -0,0 +1,33 @@
+import trustee
+
+em = trustee.ExprManager()
+
+x = em.var('x', em.bool())
+
+true_def, true = em.basic_def(em.eq_app(em.var('true', em.bool()),
+        em.eq_app(em.lam(x,x), em.  lam(x,x))))
+
+ot1 = em.parse_ot([
+  "../../ot-data/data/opentheory/bool-def-1.11/bool-def.art",
+  "../../ot-data/data/opentheory/axiom-extensionality-1.9/axiom-extensionality.art",
+  "../../ot-data/data/opentheory/axiom-choice-1.8/axiom-choice.art",
+  "../../ot-data/data/opentheory/bool-int-1.18/bool-int.art",
+  "../../ot-data/data/opentheory/bool-ext-1.12/bool-ext.art",
+  "../../ot-data/data/opentheory/bool-class-1.26/bool-class.art",
+  "../../ot-data/data/opentheory/function-def-1.20/function-def.art",
+  "../../ot-data/data/opentheory/function-thm-1.49/function-thm.art",
+  "../../ot-data/data/opentheory/axiom-infinity-1.12/axiom-infinity.art",
+  "../../ot-data/data/opentheory/pair-def-1.24/pair-def.art",
+  "../../ot-data/data/opentheory/pair-thm-1.31/pair-thm.art",
+  "../../ot-data/data/opentheory/natural-def-1.29/natural-def.art",
+  "../../ot-data/data/opentheory/natural-thm-1.22/natural-thm.art",
+  #"../../ot-data/data/opentheory/natural-order-def-1.33/natural-order-def.art",
+  "../../ot-data/data/opentheory/set-def-1.52/set-def.art",
+  #"../../ot-data/data/opentheory/set-thm-1.75/set-thm.art",
+  #"../../ot-data/data/opentheory/natural-order-thm-1.41/natural-order-thm.art",
+  #"../../ot-data/data/opentheory/natural-add-def-1.25/natural-add-def.art",
+    ])
+
+print(f"num axioms: {len(ot1[1])}")
+for x in ot1[1]:
+    print(x)
