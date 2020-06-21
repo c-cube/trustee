a55ed92be88b5f87c6109aac2f24ddbcb4fb2c12
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-06-21 17:11:38 -0400

    refactor: delete database

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 pub mod algo;
-pub mod database;
 mod fnv;
 pub mod kernel_of_trust;
 pub mod open_theory;
@@ -7,7 +6,6 @@ pub mod syntax;
 
 pub use fnv::{new_set_with_cap, new_table_with_cap, FnvHashMap, FnvHashSet};
 
-pub use database::Database;
 pub use kernel_of_trust::ExprView::*;
 pub use kernel_of_trust::{
     Ctx, CtxI, Error, Expr, ExprView, Result, Symbol, Thm, Var,
