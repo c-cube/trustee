bc84f2dca2b277fe5f2d1a37327740bd86658427
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-07-20 18:25:07 -0400

    fix to example

--- ocaml/examples/load_ot.ml
+++ ocaml/examples/load_ot.ml
@@ -1,9 +1,11 @@
 
 let () =
-  let ctx = Trustee.Ctx.create() in
-  let (a,b,c) = Trustee.OpenTheory.parse_ot_file ctx
+  let file =
+    try Sys.argv.(1) with _ ->
       "ot-data/data/opentheory/bool-def-1.11/bool-def.art"
   in
+  let ctx = Trustee.Ctx.create() in
+  let (a,b,c) = Trustee.OpenTheory.parse_ot_file ctx file in
   List.iter (fun x -> Format.printf "const: %a@." Trustee.Expr.pp x) a;
   List.iter (fun x -> Format.printf "axiom: %a@." Trustee.Thm.pp x) b;
   List.iter (fun x -> Format.printf "theorem: %a@." Trustee.Thm.pp x) c;
