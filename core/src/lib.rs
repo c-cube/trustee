28cc9c7d4edc13f6f844136a983a3e8fa6abe885
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-08-08 18:02:42 -0400

    feat(lib): reexpose some of `meta` functions

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -8,6 +8,7 @@ pub mod syntax;
 
 pub use fnv::{new_set_with_cap, new_table_with_cap, FnvHashMap, FnvHashSet};
 
+pub use crate::meta::{load_prelude_hol, run_code};
 pub use kernel_of_trust::ExprView::*;
 pub use kernel_of_trust::{Ctx, Error, Expr, ExprView, Result, Symbol, Thm, Var};
 pub use syntax::{parse_expr, parse_expr_with_args};
