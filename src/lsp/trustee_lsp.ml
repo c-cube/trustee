34270d06b525c1182b50d63ce9b7c2e0fb525424
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-04-10 00:15:40 -0400

    refactor: use linol for trustee-lsp

--- src/lsp/trustee_lsp.ml
+++ src/lsp/trustee_lsp.ml
@@ -8,9 +8,10 @@ module PA = Trustee_syntax.Parse_ast
 module TA = Trustee_syntax.Type_ast
 module Syntax = Trustee_syntax.Syntax
 
-open Lsp_lwt
-open Task.Infix
-type 'a m = 'a Task.m
+module Linol = Linol.Make(Linol.Blocking_IO)
+module Position = Linol.Position
+module Range = Linol.Range
+module LP = Lsp.Types
 
 let lsp_pos_of_pos (p:T.Position.t) : Position.t =
   Position.create ~line:(p.line-1) ~character:(p.col-1)
@@ -44,16 +45,14 @@ let ident_under_pos ~file (s:string) (pos:T.Position.t) : (string * Loc.t) optio
   in
   find()
 
-class trustee_server =
-  let _ctx = K.Ctx.create () in
-  object(self)
-    inherit Lsp_lwt.Jsonrpc2.server
+let trustee_server _ctx = object (self)
+    inherit Linol.server
 
     (* one env per document *)
-    val buffers: (DocumentUri.t, parsed_buffer) Hashtbl.t = Hashtbl.create 32
+    val buffers: (LP.DocumentUri.t, parsed_buffer) Hashtbl.t = Hashtbl.create 32
 
     method private _on_doc
-        ~(notify_back:Lsp_lwt.Jsonrpc2.notify_back) (d:DocumentUri.t) (content:string) =
+        ~(notify_back:Linol.notify_back) (d:LP.DocumentUri.t) (content:string) =
       (* TODO: use penv/env from dependencies, if any, once we have import *)
 
       let penv = PA.Env.create () in
@@ -74,16 +73,16 @@ class trustee_server =
                    let message = Fmt.asprintf "@[info: %a@]" msg() in
                    Log.debugf 5 (fun k->k"LSP info loc: %a"
                                     Yojson.Safe.pp (Range.yojson_of_t range));
-                   let d = Diagnostic.create
-                       ~severity:DiagnosticSeverity.Information
+                   let d = LP.Diagnostic.create
+                       ~severity:LP.DiagnosticSeverity.Information
                        ~range ~message () in
                    diags := d :: !diags
                  )
                ~on_error:(fun loc e ->
                    let range = lsp_range_of_loc loc in
                    let message = Fmt.asprintf "@[err: %a@]" e() in
-                   let d = Diagnostic.create
-                       ~severity:DiagnosticSeverity.Error ~range ~message () in
+                   let d = LP.Diagnostic.create
+                       ~severity:LP.DiagnosticSeverity.Error ~range ~message () in
                    diags := d :: !diags
                  )
                stmt)
@@ -97,25 +96,25 @@ class trustee_server =
       Log.debugf 2 (fun k->k"send back %d diagnostics" (List.length diags));
       notify_back#send_diagnostic diags
 
-    method on_notif_doc_did_open ~notify_back d ~content : unit m =
+    method on_notif_doc_did_open ~notify_back d ~content : unit =
       Log.debugf 2 (fun k->k "did open %s (%d bytes)" d.uri (String.length content));
       self#_on_doc ~notify_back d.uri content
 
-    method on_notif_doc_did_close ~notify_back:_ d : unit m =
+    method on_notif_doc_did_close ~notify_back:_ d : unit =
       Log.debugf 2 (fun k->k "did close %s" d.uri);
       Hashtbl.remove buffers d.uri;
-      Lwt.return ()
+      ()
 
-    method on_notif_doc_did_change ~notify_back d _c ~old_content:_old ~new_content : unit m =
+    method on_notif_doc_did_change ~notify_back d _c ~old_content:_old ~new_content : unit =
       Log.debugf 5 (fun k->k "did update %s (%d bytes -> %d bytes)" d.uri
                    (String.length _old) (String.length new_content));
       self#_on_doc ~notify_back d.uri new_content
 
     (* ## requests ## *)
 
-    method! on_req_hover ~uri ~pos (_d:Jsonrpc2.doc_state) : _ m =
+    method! on_req_hover ~notify_back:_ ~uri ~pos (_d:Linol.doc_state) : _ option =
       match Hashtbl.find buffers uri with
-      | exception Not_found -> Lwt.return None
+      | exception Not_found -> None
       | {idx; _} ->
         let pos = pos_of_lsp_pos pos in
         Log.debugf 1
@@ -127,17 +126,17 @@ class trustee_server =
           | q :: _ ->
             Log.debugf 5 (fun k->k"found %a" q#pp ());
             let s = Fmt.to_string q#pp () in
-            let r = Hover.create
-                ~contents:(`MarkedString {MarkedString.value=s; language=None})
+            let r = LP.Hover.create
+                ~contents:(`MarkedString {LP.MarkedString.value=s; language=None})
                 ~range:(lsp_range_of_loc q#loc) ()
             in
             Some r
         in
-        Lwt.return r
+        r
 
-    method! on_req_definition ~uri ~pos _st : _ option Lwt.t =
+    method! on_req_definition ~notify_back:_ ~uri ~pos _st : _ option =
       match Hashtbl.find buffers uri with
-      | exception Not_found -> Lwt.return None
+      | exception Not_found -> None
       | {idx;_} ->
         let pos = pos_of_lsp_pos pos in
         Log.debugf 1
@@ -152,22 +151,22 @@ class trustee_server =
             | Some loc ->
               Log.debugf 5 (fun k->k"found def at %a" T.Loc.pp q#loc);
               let loc =
-                Location.create ~uri:loc.T.Loc.file ~range:(lsp_range_of_loc loc) in
+                LP.Location.create ~uri:loc.T.Loc.file ~range:(lsp_range_of_loc loc) in
               let r = `Location [loc] in
               (* Log.debugf 20 (fun k->k"response: %a" Yojson.Safe.pp (Locations.yojson_of_t r)); *)
               Some r
         in
-        Lwt.return r
+        r
 
-    method! on_req_completion ~uri ~pos ~ctx:_ doc_st : _ option Lwt.t =
+    method! on_req_completion ~notify_back:_ ~uri ~pos ~ctx:_ doc_st : _ option =
       match Hashtbl.find buffers uri with
-      | exception Not_found -> Lwt.return None
+      | exception Not_found -> None
       | {idx;_} ->
         let pos = pos_of_lsp_pos pos in
         Log.debugf 5 (fun k->k"completion at %a" T.Position.pp pos);
         (* find token under the cursor, if any *)
         begin match ident_under_pos ~file:uri doc_st.content pos with
-          | None -> Lwt.return None
+          | None -> None
           | Some (ident, ident_loc) ->
             Log.debugf 5 (fun k->k"req-completion: ident `%s`" ident);
 
@@ -186,29 +185,29 @@ class trustee_server =
               |> Iter.map
                 (fun (name, c) ->
                   let lbl, kind = match c with
-                    | TA.Ty_env.N_const _ -> "C", CompletionItemKind.Value
-                    | TA.Ty_env.N_thm _ -> "T", CompletionItemKind.Value
-                    | TA.Ty_env.N_rule  _ -> "R", CompletionItemKind.Operator
+                    | TA.Ty_env.N_const _ -> "C", LP.CompletionItemKind.Value
+                    | TA.Ty_env.N_thm _ -> "T", LP.CompletionItemKind.Value
+                    | TA.Ty_env.N_rule  _ -> "R", LP.CompletionItemKind.Operator
                   in
                   let label = Printf.sprintf "%s %s" lbl name in
                   let textEdit =
-                    TextEdit.create ~range:(lsp_range_of_loc ident_loc)
+                    LP.TextEdit.create ~range:(lsp_range_of_loc ident_loc)
                       ~newText:name
                   in
-                  let ci = CompletionItem.create
+                  let ci = LP.CompletionItem.create
                     ~label ~kind ~textEdit
                     ~detail:(TA.Ty_env.string_of_named_object c)
                     ()
                   in
                   Log.debugf 50
                     (fun k->k"compl item: %a"
-                        Yojson.Safe.pp (CompletionItem.yojson_of_t ci));
+                        Yojson.Safe.pp (LP.CompletionItem.yojson_of_t ci));
                   ci
                 )
               |> Iter.to_list
             in
             Log.debugf 5 (fun k->k"send back %d completions" (List.length compls));
-            Lwt.return (Some (`List compls))
+            Some (`List compls)
         end
   end
 
@@ -223,25 +222,13 @@ let setup_logger_ () =
 
 let () =
   setup_logger_ ();
-  let s = new trustee_server in
-  (* TODO: the task is the LSP server *)
-  let task =
-    Task.start ~descr:"top task"
-      (fun _top_task ->
-         Log.debugf 1 (fun k->k"start lsp");
-         let server = Jsonrpc2.create_stdio s in
-         let* () =
-           Task.run_sub ~descr:"lsp server"
-             ~parent:_top_task (fun _ -> Jsonrpc2.run server _top_task)
-           >>= Task.unwrap
-         in
-         Task.return ()
-      )
-  in
-  match Task.run task with
-  | Error e ->
+  let ctx = K.Ctx.create() in
+  let server = trustee_server ctx in
+  let task = Linol.create_stdio server in
+  match Linol.run task with
+  | exception e ->
     let e = Printexc.to_string e in
     Log.debugf 1 (fun k->k"error: %s" e);
     Printf.eprintf "error: %s\n%!" e;
     exit 1
-  | Ok () -> ()
+  | () -> ()
