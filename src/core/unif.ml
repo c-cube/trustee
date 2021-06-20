919ead1062d4d32305d08d480b3fd72b4d62a249
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-06-20 13:09:15 -0400

    fix(unif): no occur check needed

--- src/core/unif.ml
+++ src/core/unif.ml
@@ -23,12 +23,10 @@ let rec occ_check subst v e =
   | E.E_var v' -> K.Var.equal v v'
   | _ -> E.exists e ~f:(fun _ e' -> occ_check subst v e')
 
-type op = O_unif | O_match
-
-let u_rec op subst a b : subst =
+let unif_rec_ subst a b : subst =
   let rec loop subst a b =
     let a = deref subst a in
-    let b = if op == O_unif then deref subst b else b in
+    let b = deref subst b in
 
     (* unify types first *)
     let subst = match E.ty a, E.ty b with
@@ -43,7 +41,7 @@ let u_rec op subst a b : subst =
     | E.E_var v, _ ->
       if occ_check subst v b then raise Fail;
       Su.bind subst v b
-    | _, E.E_var v when op == O_unif ->
+    | _, E.E_var v ->
       if occ_check subst v a then raise Fail;
       Su.bind subst v a
     | E.E_const (c1, l1), E.E_const (c2, l2) when K.Const.equal c1 c2 ->
@@ -64,13 +62,45 @@ let u_rec op subst a b : subst =
   in
   loop subst a b
 
-let unify_exn ?(subst=Su.empty) a b = u_rec O_unif subst a b
+let unify_exn ?(subst=Su.empty) a b = unif_rec_ subst a b
 
 let unify ?subst a b =
   try Some (unify_exn ?subst a b)
   with Fail -> None
 
-let match_exn ?(subst=Su.empty) a b = u_rec O_match subst a b
+let match_rec_ subst a b : subst =
+  let rec loop subst a b =
+    let a = deref subst a in
+    (* match types first *)
+    let subst = match E.ty a, E.ty b with
+      | None, None -> subst
+      | Some ty1, Some ty2 when E.equal ty1 ty2 -> subst
+      | Some ty1, Some ty2 -> loop subst ty1 ty2
+      | Some _, None | None, Some _ -> raise Fail
+    in
+    match E.view a, E.view b with
+    | _ when E.equal a b -> subst
+    | E.E_var v1, E.E_var v2 when K.Var.equal v1 v2 -> subst
+    | E.E_var v, _ -> Su.bind subst v b
+    | E.E_const (c1, l1), E.E_const (c2, l2) when K.Const.equal c1 c2 ->
+      assert (List.length l1=List.length l2);
+      List.fold_left2 loop subst l1 l2
+    | E.E_bound_var v1, E.E_bound_var v2 when v1.bv_idx = v2.bv_idx ->
+      subst (* types are already matched *)
+    | E.E_app (f1, arg1), E.E_app (f2, arg2)
+    | E.E_arrow (f1, arg1), E.E_arrow (f2, arg2) ->
+      let subst = loop subst f1 f2 in
+      loop subst arg1 arg2
+    | E.E_lam (_, ty1, bod1), E.E_lam (_, ty2, bod2) ->
+      let subst = loop subst ty1 ty2 in
+      loop subst bod1 bod2
+    | (E.E_kind | E.E_type | E.E_app _ | E.E_arrow _
+      | E.E_const _ | E.E_bound_var _ | E.E_lam _), _ ->
+      raise Fail
+  in
+  loop subst a b
+
+let match_exn ?(subst=Su.empty) a b = match_rec_ subst a b
 
 let match_ ?subst a b =
   try Some (match_exn ?subst a b)
--- tests/core/test_unif.ml
+++ tests/core/test_unif.ml
@@ -15,6 +15,7 @@ module Make() = struct
   let c_g2 = new_const "g2" (tau @-> tau @-> tau)
   let x = var_name "x" tau
   let y = var_name "y" tau
+  let z = var_name "z" tau
   let f1 =
     let f = const c_f1 [] in
     fun x -> app_l f [x]
@@ -37,7 +38,7 @@ let assert_eq_expr t1 t2 =
   assert_equal ~cmp:E.equal
     ~printer:(Fmt.to_string @@ Fmt.within"`" "`" E.pp) t1 t2
 
-let reg1 ctxt =
+let reg1 _ctxt =
   let module M = Make() in let open M in
   let t1 = f2 x y in
   let t2 = f2 a x in
@@ -45,8 +46,19 @@ let reg1 ctxt =
   Format.eprintf "subst %a@." K.Subst.pp subst;
   assert_eq_expr (E.subst ~recursive:false t1 subst) t2
 
+let reg2 _ctxt =
+  let module M = Make() in let open M in
+  let lhs = f2 x y in
+  let rhs = f2 y x in
+  let t2 = f2 (f2 x y) z in
+  let t2' = f2 z (f2 x y) in
+  let subst = Unif.match_ lhs t2 |> assert_is_some "must match" in
+  Format.eprintf "subst %a@." K.Subst.pp subst;
+  assert_eq_expr (E.subst ~recursive:false rhs subst) t2'
+
 let suite =
   "unif" >::: [
     "reg1" >:: reg1;
+    "reg2" >:: reg2;
   ]
 
