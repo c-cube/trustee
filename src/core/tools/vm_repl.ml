bbae3d6de11c344242f676fcc17d9798de004bc9
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-03-13 17:16:43 -0400

    fixes in VM; add `mult`; add debug hook; improve repl

--- src/core/tools/vm_repl.ml
+++ src/core/tools/vm_repl.ml
@@ -33,11 +33,45 @@ let prims = [
 ] |> List.map (fun p -> VM.Primitive.name p, p)
   |> Str_map.of_list
 
-let () =
-  Sys.catch_break true;
-  Readline.with_ @@ fun () ->
+let help =
+  "VM repl for Trustee.\n\
+   Each line is interpreted separately.\n\
+   \n\
+   Special commands:\n\
+  \ - 'help' to display this\n\
+  \ - 'reset' to reset the VM\n\
+  \ - 'dump' to display the VM's state\n\
+  \ - 'set x …' sets 'x' to the (top) result of evaluating the RHS\n\
+  "
 
+let debug = ref false
+let readline = ref true
+
+let debug_hook vm i =
+  Fmt.eprintf "@[<2>exec `%a`@ in %a@]@." VM.Instr.pp i VM.dump vm
+
+let main () =
   let vm = VM.create() in
+  if !debug then VM.set_debug_hook vm debug_hook;
+
+  let parse_str str =
+    match VM.parse_string ~prims str  with
+    | Error err ->
+      Fmt.printf "Error: %a@." Error.pp err;
+      None
+    | Ok c -> Some c
+  in
+
+  let eval_chunk ~vm c =
+    if !debug then Fmt.eprintf "### eval chunk@.";
+    try
+      VM.run vm c;
+    with
+    | Error.E err ->
+      Fmt.eprintf "%a@." Error.pp err;
+    | e ->
+      Printf.eprintf "exception: %s\n%!" (Printexc.to_string e)
+  in
 
   let continue = ref true in
   while !continue do
@@ -48,25 +82,50 @@ let () =
 
     | Some "reset" -> VM.reset vm;
     | Some "dump" -> Fmt.printf "%a@." VM.dump vm;
+    | Some ("help" as str) -> Readline.hist_add str; print_endline help;
+
+    | Some line when CCString.prefix ~pre:"set " line ->
+      Readline.hist_add line;
+
+      let _, rest = CCString.Split.left_exn ~by:" " line in
+      let name, rest = CCString.Split.left_exn ~by:" " rest in
+      let name = String.trim name in
+
+      begin match parse_str rest with
+        | None -> ()
+        | Some c ->
+
+          (* run [c] in a different VM to get the value *)
+          let vm' = VM.create ~env:(VM.get_env vm) () in
+          if !debug then VM.set_debug_hook vm' debug_hook;
+
+          eval_chunk ~vm:vm' c;
+
+          (* assign result of evaluation to [k] *)
+          let v = VM.pop_exn vm' in
+          VM.set_env vm (VM.get_env vm |> VM.Env.add name v);
+          Fmt.printf "defined %S@." name;
+      end;
 
     | Some line ->
-      (* FIXME: have a syntax to modify env, like `SET foo …` *)
-      begin match VM.parse_string ~prims line with
-        | Error err ->
-          Fmt.printf "Error: %a@." Error.pp err
-        | Ok c ->
-          Readline.hist_add line;
-          begin
-            try
-              VM.run vm c;
-            with
-            | Error.E err ->
-              Fmt.eprintf "%a@." Error.pp err;
-            | e ->
-              Printf.eprintf "exception: %s\n%!" (Printexc.to_string e)
-          end
+      Readline.hist_add line;
+
+      begin match parse_str line with
+        | None ->()
+        | Some c -> eval_chunk ~vm c
       end
     | None -> continue := false
     | exception Sys.Break -> continue := false
   done;
   ()
+
+let () =
+  Sys.catch_break true;
+
+  let args = [
+    "--raw", Arg.Clear readline, " disable readline";
+    "--debug", Arg.Set debug, " enable debug";
+  ] |> Arg.align in
+  Arg.parse args ignore "repl [opt*]";
+
+  if !readline then Readline.with_ main else main()
