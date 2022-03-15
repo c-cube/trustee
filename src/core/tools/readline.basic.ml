99126699df8c64555649bc9e20021f12e12a59f9
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-03-14 21:39:48 -0400

    readline: only print prompt if enabled

--- src/core/tools/readline.basic.ml
+++ src/core/tools/readline.basic.ml
@@ -1,9 +1,10 @@
 
 
-let init () = ()
+let enabled_ = ref false
+let init () = enabled_ := true
 let hist_add _ = ()
-let with_ f = f()
+let with_ f = init(); f()
 let read_line pr =
-  Printf.printf "%s%!" pr;
+  if !enabled_ then Printf.printf "%s%!" pr;
   try Some (input_line stdin)
   with End_of_file -> None
