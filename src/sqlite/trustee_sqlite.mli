dc0eec4664059a2b471a9788eea785e69de0eb2d
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-12-19 20:29:13 -0500

    remove dead code

--- src/sqlite/trustee_sqlite.mli
+++
@@ -1,5 +0,0 @@
-val storage : string -> Storage.t
-(** Use the given filename to provide storage using Sqlite *)
-
-val storage_xdg_cache : unit -> Storage.t
-(** Use the XDG cache directory to provide storage using Sqlite *)
