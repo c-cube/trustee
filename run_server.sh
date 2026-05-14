89db9f8eb03612519ffa0e74e8f8fbd6021b65eb
Simon Cruanes <simon.cruanes.2007@m4x.org>
2026-05-13 21:07:52 -0400

    fix script

--- run_server.sh
+++ run_server.sh
@@ -2,5 +2,5 @@
 
 eval $( opam env )
 exec dune exec --profile=release ./src/opentheory/tool/trustee_ot.exe \
-  -- --serve --port 8089 --proofs-zip ot-data.zip $@
+  -- --serve --port 8089 --proof-zip ot-data.zip $@
 
